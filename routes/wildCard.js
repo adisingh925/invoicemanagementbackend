4d51ccd37c7a6c65c66e6bd2a952bb82b465f457
adisingh925 <adisingh925@gmail.com>
2024-02-17 21:46:15 +0530

    na

--- routes/wildCard.js
+++ routes/wildCard.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 /**
- * Handling endpoint not found
+ * Handling endpoint not found for all types of requests
  */
 router.all("*", (_req, res) => {
   return res.status(500).json({ code: -1, msg: "Internal Server Error!" });
