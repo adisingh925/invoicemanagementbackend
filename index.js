d6720d18760dcbd5f52988e06753bea466f008c4
adisingh925 <adisingh925@gmail.com>
2024-02-17 17:24:01 +0530

    na

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 "strict mode";
 
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 const http = require("http");
 require("dotenv").config();
-const port = process.env.PORT
+const port = process.env.PORT;
 
 app.use(express.json());
 
@@ -18,7 +18,7 @@ app.use("/", require("./routes/wildCard"));
 const httpServer = http.createServer(app);
 
 /**
- * starting https server
+ * starting http server
  */
 httpServer.listen(process.env.HTTP_PORT, () => {
   console.log(`HTTP Server running on port ${process.env.HTTP_PORT}`);
