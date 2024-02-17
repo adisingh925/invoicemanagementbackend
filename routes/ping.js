0fc4976e1235061411912ddf9199bd660248ae68
adisingh925 <adisingh925@gmail.com>
2024-02-17 17:36:49 +0530

    na

--- routes/ping.js
+++ routes/ping.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
+/**
+ * @route GET /ping
+ */
 router.get("/", (_req, res) => {
   return res.status(200).json({ code: 1, msg: "Pong!" });
 });
