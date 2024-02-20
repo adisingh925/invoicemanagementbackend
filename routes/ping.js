072f3ec2ebf18b55fde70312a5feba2d9977871e
adisingh925 <adisingh925@gmail.com>
2024-02-20 22:08:12 +0530

    na

--- routes/ping.js
+++ routes/ping.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 const router = Router();
+import { pingRateLimiter } from "../ratelimiters/rateLimiters.js";
 
 /**
  * @route GET /ping
  */
-router.get("/", (_req, res) => {
+router.get("/", pingRateLimiter, (_req, res) => {
   return res.status(200).json({ code: 1, msg: "Pong!" });
 });
 
