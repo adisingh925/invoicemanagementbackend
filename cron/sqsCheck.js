b3e38293aa5ba395edfde72a085dcee2829d3c0d
adisingh925 <adisingh925@gmail.com>
2024-02-24 22:58:39 +0530

    na

--- cron/sqsCheck.js
+++ cron/sqsCheck.js
@@ -1,6 +1,6 @@
 import cron from "node-cron";
 import { fetchSingleMessage } from "../aws/sns.js";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
 cron.schedule("* * * * *", () => {
