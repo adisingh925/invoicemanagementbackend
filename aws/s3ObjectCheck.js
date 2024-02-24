99a40452699e234c50a83550a7e3dc5e8516f2c8
adisingh925 <adisingh925@gmail.com>
2024-02-25 00:52:22 +0530

    na

--- aws/s3ObjectCheck.js
+++ aws/s3ObjectCheck.js
@@ -5,7 +5,7 @@ const s3Client = new S3Client({ region: process.env.AWS_REGION });
 export const fetchAndCheckObjectMetadata = async (message) => {
   try {
     const params = {
-      Bucket: message.bucketName,
+      Bucket: process.env.AWS_S3_BUCKET_NAME,
       Key: message.key,
     };
 
