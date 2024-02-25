8c5301bbccbbe2e9ecd38242b503ccfc0b63b594
adisingh925 <adisingh925@gmail.com>
2024-02-26 02:08:44 +0530

    na

--- parser/pdfParser.js
+++ parser/pdfParser.js
@@ -2,15 +2,17 @@ import { PdfReader } from "pdfreader";
 
 export const parsePdf = async (path) => {
   return new Promise((resolve, reject) => {
+    const textItems = [];
+
     new PdfReader().parseFileItems(path, async (err, item) => {
       if (err) {
         console.error("parsePdf() => ", err.message);
         reject(err.message);
       } else if (!item) {
         console.warn("parsePdf() => end of file!");
-        reject("end of file!");
+        resolve(textItems.join(" "));
       } else if (item.text) {
-        resolve(item.text);
+        textItems.push(item.text);
       }
     });
   });
