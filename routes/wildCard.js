2426326100f72a60d4cdfc9c2098fe925fc7329b
adisingh925 <adisingh925@gmail.com>
2024-04-16 01:07:32 +0530

    na

--- routes/wildCard.js
+++ routes/wildCard.js
@@ -1,12 +1,19 @@
 import express from "express";
 import { wildcardRateLimiter } from "../ratelimiters/rateLimiters.js";
+import logger from "../logging/winston.js";
 const router = express.Router();
 
 /**
  * Handling endpoint not found for all types of requests
  */
 router.all("*", wildcardRateLimiter, (_req, res) => {
-  return res.status(500).json({ code: -1, msg: "Internal Server Error!" });
+  try {
+    logger.info("Endpoint not found, Returning response");
+    return res.status(500).json({ code: -1, msg: "Internal Server Error!" });
+  } catch (error) {
+    logger.error(error);
+    return res.status(500).json({ code: -1, msg: "Internal Server Error!" });
+  }
 });
 
 export default router;
